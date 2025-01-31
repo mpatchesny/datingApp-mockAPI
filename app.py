01bab9ee916a39a8e40e24e5b47d780dc9938e29
MPaczesny <maciej.paczesny@predictx.com>
2025-01-31 12:44:56 +0000

    add Authorization to CORS allowed headers

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def enable_cors():
     """
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, PATCH, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token, Authorization'
 
 @app.route('/api')
 def hello():
